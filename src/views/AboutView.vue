07c25b45892604f9c87806f1fa4778fc03db37fc
Joakim Lundell <jocke@bluestripes.se>
2023-09-06 12:16:41 +0200

    correct spelling

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,8 +3,8 @@
         <h1>About</h1>
         <h2>The JavaScript assignment</h2>
         <p>This application let's you mark three points and create a parallelogram from them. 
-            It also calculates the area of the parallelogram and generates a yellow circle with tha same size, 
-            located in the center of the parallelogram.</p>
+            It also calculates the area of the parallelogram and generates a yellow circle with
+             the same size, located in the center of the parallelogram.</p>
         <h2>How to use</h2>
         <ol>
             <li>Got to startpage</li>
@@ -14,7 +14,8 @@
             <li>Or, click on reset to start over</li>
         </ol>
         <h2>Created by</h2>
-        <p>Joakim Lundell, full-stack developer with a special eye on the front-end. Has over 20 years experience as a programmer but struggled a bit more with this task than he wants to admit :)</p>
+        <p>Joakim Lundell, full-stack developer with a special eye on front-end. Has over 20 years
+             experience as a programmer but struggled a bit more with this task than he wants to admit :)</p>
         <h2>Credits</h2>
         <p>Thanks to VetFamily for coming up with this great fun assignment.</p>
         <p>Regards, <i>Joakim Lundell</i></p>
@@ -23,6 +24,6 @@
 
 <style>
 .about {
-    padding: calc(var(--header-height) + var(--padding)) var(--padding)
+    padding: calc(var(--header-height) + var(--padding)) var(--padding);
 }
 </style>
